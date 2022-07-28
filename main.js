3f91b44cdd68312cf07faa9a21a0a8fef9c3d57f
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-28 15:26:05 -0600

    Added functions to display card

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var titleInput = document.querySelector('.title')
 var bodyInput = document.querySelector('.bodyForm')
 var saveButton = document.querySelector('.saveButton')
 var ideaGrid = document.querySelector('.ideaCardsGrid')
-
+var cardsContainer = document.querySelector('.cardsContainer')
 
 
 
@@ -14,7 +14,7 @@ var ideaGrid = document.querySelector('.ideaCardsGrid')
 
 var collection = [];
 //Event Listeners------------>
-saveButton.addEventListener('click', createIdea);
+saveButton.addEventListener('click', addCard);
 
 
 
@@ -32,8 +32,32 @@ saveButton.addEventListener('click', createIdea);
 function createIdea() {
   var newCard = new Idea(titleInput.value, bodyInput.value);
   collection.push(newCard)
-  console.log(collection[0])
-  // return newCard
+
+  return newCard
+}
+
+function addCard(){
+  var newIdea = createIdea()
+  makeCard(newIdea)
+    console.log(newIdea.title)
+}
+
+function makeCard(newIdea){
+  cardsContainer.insertAdjacentHTML("afterbegin", `<article class='ideaCardsGrid' id='${newIdea.id}'>
+    <header>
+      <button class='starButton' id='starButton'><img src='Assets/star.svg' width='35px'/></button>
+      <button class='deleteButton' id='deleteButton'><img src='Assets/delete.svg' width='35px'/></button>
+    </header>
+    <section class='cardBody'>
+      <h2>${newIdea.title}</h2>
+      <p>${newIdea.body}</p>
+    </section>
+    <footer>
+      <button type='button' class='commentButton' id='commentButton'><img src='Assets/comment.svg' width= '35px'/></button>
+      <label for='commentButton'>Comment</label>
+    </footer>
+  </article>`
+  )
 }
 
 //We want to instantiate a new Idea class with the input value from titleInput and bodyInput.
